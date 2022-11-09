a622c80e81aecddbe5cb10700a5e12e660f14997
Diego1029ivan <dialvaradov@alumno.unsm.edu.pe>
2022-11-09 16:28:21 -0500

    nuevo
    h

--- usuarios_est.php
+++ usuarios_est.php
@@ -1,3 +1,22 @@
+<?php
+    $urlapi="http://localhost:75/api/";
+	$curl = curl_init();
+
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $urlapi.'usuarios',
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'GET',
+    ));
+	$response = curl_exec($curl);
+	curl_close($curl);
+	$data = json_decode($response); 
+    var_dump($data);
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -436,20 +455,22 @@
                                             <th>Modificar</th>
                                         </tr>
                                     </tfoot>
+                                    <?php foreach ($data as $usuario) : ?>
                                     <tbody>
                                         <tr>
-                                            <td>Tiger Nixon</td>
-                                            <td>System Architect</td>
-                                            <td>correo1@hotmail.com</td>
-                                            <td>613537664</td>
-                                            <td>usuario1</td>
-                                            <td>pass1</td>
+                                            <td><?= $usuario->nombre?></td>
+                                            <td><?= $usuario->apellido?></td>
+                                            <td><?= $usuario->email?></td>
+                                            <td><?= $usuario->celular?></td>
+                                            <td><?= $usuario->password?></td>
+                                            <td><?= $usuario->username?></td>
                                             <td>activo</td>
                                             <td class="botones-tabla">
                                                 <a href="#"><i class="fas fa-pencil-alt"></i></a>
                                                 <a href="#"><i class="fas fa-trash-alt"></i></a>
                                             </td>
                                         </tr>
+                                    <?php endforeach ?>	
                                         <tr>
                                             <td>Garrett Winters</td>
                                             <td>Accountant</td>
